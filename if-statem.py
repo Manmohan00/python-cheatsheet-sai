a284b079046463703275aa9d4fdf42a041df71ab
Sai Vishnu <saivishnu725@gmail.com>
2022-05-13 22:48:27 +0530

    commit_message

---
+++ if-statem.py
@@ -0,0 +1,17 @@
+x = 10
+if x > 5:
+    print("It is greater.")
+
+y = 20
+if x > y:
+    print("if")
+else:
+    print("else")
+
+z = 20
+if y < z:
+    print("Less")
+elif y > z:
+    print("More")
+else:
+    print("Equal")
