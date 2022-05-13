e1742b6eacc85a22639084f03168b3f9c4b72bdc
Sai Vishnu <saivishnu725@gmail.com>
2022-05-13 19:09:42 +0530

    String stuffs

---
+++ strings.py
@@ -0,0 +1,14 @@
+print('Hello World!')
+
+# New line
+print('Hello \nNew line World!')
+
+# Write multiple lines of strings using three double quotes
+print("""\nHello
+World""")
+
+# Concat
+print("Hello " + "World!")
+
+# Multiply string by a int to get the same string int number of times
+print("Hello " * 2)
